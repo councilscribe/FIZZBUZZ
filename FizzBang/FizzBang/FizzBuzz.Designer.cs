3b41ca12707bca03ec7c764bda9e26e5f9398882
councilscribe <councilscribe@gmail.com>
2018-05-30 14:00:39 +0800

    FizzBuzzSample

---
+++ FizzBang/FizzBang/FizzBuzz.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FizzBang
+{
+    partial class FizzBuzz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(108, 12);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(112, 23);
+            this.btnCompute.TabIndex = 0;
+            this.btnCompute.Text = "FizzBuzzAway";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(229, 537);
+            this.listBox1.TabIndex = 2;
+            // 
+            // FizzBuzz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 589);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCompute);
+            this.Name = "FizzBuzz";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FizzBang/FizzBang/FizzBuzz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBang
+{
+    public partial class FizzBuzz : Form
+    {
+        public FizzBuzz()
+        {
+            InitializeComponent();
+        }
+
+        public void FizzBuzzGo()
+        {
+            Boolean Fizz = false;
+            Boolean Buzz = false;
+            for (int count = 1; count <= 100; count ++)
+            {
+                Fizz = count % 3 == 0;
+                Buzz = count % 5 == 0;
+                if (Fizz && Buzz)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                    listBox1.Items.Add("Fizz Buzz");
+                }
+                else if (Fizz)
+                {
+                    Console.WriteLine("Fizz");
+                    listBox1.Items.Add("Fizz");
+                }
+                else if (Buzz)
+                {
+                    Console.WriteLine("Buzz");
+                    listBox1.Items.Add("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    listBox1.Items.Add(count);
+                }
+            }
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            FizzBuzzGo();
+        }
+    }
+}
